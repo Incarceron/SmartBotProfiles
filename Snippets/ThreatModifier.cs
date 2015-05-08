23a0df2b3cd16fa452ecc8deea2f6400b8b6fdc4
Wirmate <wirmate@gmail.com>
2015-05-08 20:41:43 +0200

    Update threatmodifier.cs

--- Snippets/ThreatModifier.cs
+++ Snippets/ThreatModifier.cs
@@ -1,114 +1,118 @@
 public float GetThreatModifier(Card card)
-		{
-			switch (card.Template.Id)
-			{
-				case Card.Cards.GVG_006://Mechwarper
-					return 4;
-					
-				case Card.Cards.FP1_013://Kel'Thuzad
+{
+	switch (card.Template.Id)
+	{
+		case Card.Cards.GVG_006://Mechwarper
+					return 7;
+
+		case Card.Cards.FP1_013://Kel'Thuzad
 					return 6;
-					
-				case Card.Cards.EX1_016://Sylvanas Windrunner
+
+		case Card.Cards.EX1_016://Sylvanas Windrunner
 					return 5;
-					
-				case Card.Cards.GVG_105://Piloted Sky Golem
+
+		case Card.Cards.GVG_105://Piloted Sky Golem
 					return 3;
-					
-				case Card.Cards.BRM_031://Chromaggus
+
+		case Card.Cards.BRM_031://Chromaggus
 					return 5;
-					
-				case Card.Cards.EX1_559://Archmage Antonidas
+
+		case Card.Cards.EX1_559://Archmage Antonidas
 					return 8;
-					
-				case Card.Cards.GVG_021://Mal'Ganis
+
+		case Card.Cards.GVG_021://Mal'Ganis
 					return 6;
-					
-				case Card.Cards.EX1_608://Sorcerer's Apprentice
-					return 3;
-					
-				case Card.Cards.NEW1_012://Mana Wyrm
-					return 3;
-					
-				case Card.Cards.BRM_002://Flamewaker
+
+		case Card.Cards.EX1_608://Sorcerer's Apprentice
+					return 5;
+
+		case Card.Cards.NEW1_012://Mana Wyrm
+					return 5;
+
+		case Card.Cards.BRM_002://Flamewaker
 					return 4;
-					
-				case Card.Cards.EX1_595://Cult Master
+
+		case Card.Cards.EX1_595://Cult Master
 					return 2;
-					
-				case Card.Cards.NEW1_021://Doomsayer
+
+		case Card.Cards.NEW1_021://Doomsayer
 					return 0;
-					
-				case Card.Cards.EX1_243://Dust Devil
+
+		case Card.Cards.EX1_243://Dust Devil
 					return 2;
-					
-				case Card.Cards.EX1_170://Emperor Cobra
+
+		case Card.Cards.EX1_170://Emperor Cobra
 					return 4;
-					
-				case Card.Cards.BRM_028://Emperor Thaurissan
+
+		case Card.Cards.BRM_028://Emperor Thaurissan
 					return 6;
-					
-				case Card.Cards.EX1_565://Flametongue Totem
+
+		case Card.Cards.EX1_565://Flametongue Totem
 					return 5;
-					
-				case Card.Cards.GVG_100://Floating Watcher
+
+		case Card.Cards.GVG_100://Floating Watcher
 					return 4;
-					
-				case Card.Cards.GVG_113://Foe Reaper 4000
+
+		case Card.Cards.GVG_113://Foe Reaper 4000
 					return 0;
-					
-				case Card.Cards.tt_004://Flesheating Ghoul
+
+		case Card.Cards.tt_004://Flesheating Ghoul
 					return 2;
-					
-				case Card.Cards.EX1_604://Frothing Berserker
+
+		case Card.Cards.EX1_604://Frothing Berserker
 					return 3;
-					
-				case Card.Cards.BRM_019://Grim Patron
+
+		case Card.Cards.BRM_019://Grim Patron
 					return 7;
-					
-				case Card.Cards.EX1_084://Warsong Commander
+
+		case Card.Cards.EX1_084://Warsong Commander
 					return 7;
-					
-				case Card.Cards.EX1_095://Gadgetzan Auctioneer
+
+		case Card.Cards.EX1_095://Gadgetzan Auctioneer
 					return 4;
-					
-				case Card.Cards.NEW1_040://Hogger
+
+		case Card.Cards.NEW1_040://Hogger
 					return 3;
-					
-				case Card.Cards.GVG_104://Hobgoblin
+
+		case Card.Cards.GVG_104://Hobgoblin
 					return 5;
-					
-				case Card.Cards.EX1_614://Illidan Stormrage
+
+		case Card.Cards.EX1_614://Illidan Stormrage
 					return 4;
-					
-				case Card.Cards.GVG_027://Iron Sensei
+
+		case Card.Cards.GVG_027://Iron Sensei
 					return 5;
-					
-				case Card.Cards.GVG_094://Jeeves
+
+		case Card.Cards.GVG_094://Jeeves
 					return 5;
-				
-				case Card.Cards.NEW1_019://Knife Juggler
+
+		case Card.Cards.NEW1_019://Knife Juggler
+					return 7;
+
+		case Card.Cards.EX1_001://Lightwarden
 					return 4;
-					
-				case Card.Cards.EX1_001://Lightwarden
-					return 4;
-					
-				case Card.Cards.EX1_563://Malygos
+
+		case Card.Cards.EX1_563://Malygos
 					return 6;
-					
-				case Card.Cards.GVG_103://Micro Machine
-					return 3;
-					
-				case Card.Cards.EX1_044://Questing Adventurer
-					return 3;
-					
-				case Card.Cards.EX1_298://Ragnaros the Firelord
-					return 6;
-					
-				case Card.Cards.GVG_037://Whirling Zap-o-matic
-					return 4;
-					
-				case Card.Cards.NEW1_020://Wild Pyromancer
+
+		case Card.Cards.GVG_103://Micro Machine
 					return 5;
-			}
-			return 0;
-		}
+
+		case Card.Cards.EX1_044://Questing Adventurer
+					return 6;
+
+		case Card.Cards.EX1_298://Ragnaros the Firelord
+					return 7;
+
+		case Card.Cards.GVG_037://Whirling Zap-o-matic
+					return 6;
+
+		case Card.Cards.NEW1_020://Wild Pyromancer
+					return 6;
+
+		case Card.Cards.GVG_013://Cogmaster
+					return 8;
+	}
+
+	return 0;
+}
